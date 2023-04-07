52770f91e35352a89b587e0ddd95d035bb0908c5
老马 <laoma@LideiMac.local>
2023-04-07 14:10:02 +0800

    rename bean condif

--- src/main/java/hoopluz/security/SecurityStarterConfig.java
+++ src/main/java/hoopluz/security/SecurityStarterConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class BeanConfig {
+public class SecurityStarterConfig {
 
   @Bean
   public Jwt getJwt(JwtProperties properties) {
